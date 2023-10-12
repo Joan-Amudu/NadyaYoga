38196236605425163ca17f94ce42ad4d242ea6ec
Joan <joan.baare@gmail.com>
2023-10-12 19:09:08 +1100

    Add and style footer with social media links

--- Nadya-yoga/src/components/Footer.jsx
+++ Nadya-yoga/src/components/Footer.jsx
@@ -1,9 +1,27 @@
 import React from 'react';
 
+function Copyright() {
+    return (
+      <div>
+        {'Copyright © '}
+        <a href="https://github.com/Joan-Amudu" rel='noopener noreferrer' target="_blank">
+          Joan Amudu
+        </a>{' '}
+        {new Date().getFullYear()}
+        {'.'}
+      </div>
+    );
+  }
+
 export default function Footer(){
     return (
-    <footer>
+    <footer className="py-3, px-2, mt-auto">
       {/* Footer content goes here */}
+        <a href='https://www.facebook.com/' rel='noopener noreferrer' target='_blank'>Facebook</a> |
+        <a href='https://www.instagram.com/' rel='noopener noreferrer' target='_blank'>Instagram</a> |
+        <a href='https://www.glo.com/' rel='noopener noreferrer' target='_blank'>Glo</a>
+        <Copyright />
+    
     </footer>
   );
 }
